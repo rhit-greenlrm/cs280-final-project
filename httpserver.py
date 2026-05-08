fcf9c854f6aee08ce16bab714cf1cb21adbf7c5d
Rachel G <rachel.m.greenlee@icloud.com>
2026-05-08 14:11:38 -0400

    initial style modifications for log in page

--- httpserver.py
+++ httpserver.py
@@ -5,15 +5,15 @@ import os
 from flask import jsonify
 from flask import request
 
-#THIS IS A TEST
-
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import verify_jwt_in_request
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
+#from flask_jwt_extended import create_access_token
+#from flask_jwt_extended import verify_jwt_in_request
+#from flask_jwt_extended import get_jwt_identity
+#from flask_jwt_extended import jwt_required
+#from flask_jwt_extended import JWTManager
 
 
+
+#old code referenced from securityintro livecoding 
 from werkzeug.utils import secure_filename
 IMAGE_FOLDER = 'public/images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
