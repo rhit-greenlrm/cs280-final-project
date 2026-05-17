9cd04af94257de9e71694d21ecab9a2972c164fb
Rachel G <rachel.m.greenlee@icloud.com>
2026-05-17 13:19:04 -0400

    add_pet form

--- public/scripts/addPet.js
+++ public/scripts/addPet.js
@@ -1,1 +1,7 @@
-//TODO
+/* addPet TODOS:
+* display add new pet banner (VISUAL)
+* create form for uploading a pet 
+* full functionality for the form
+* adding to database and displaying in the list afterwards
+* should return the user to the employee dashboard after form submittion
+*/
--- public/scripts/loginScript.js
+++ public/scripts/loginScript.js
@@ -1,7 +1,8 @@
 
 /*LogIn TODOS: 
-* login button should go to either dashboard or employee action page
-* create account button should create a new account where user data can be stored
-* inputs and buttons should be centered on the page
-* fake logo image should be displayed 
+* modify button.text.input sizes (VISUAL)
+* fake logo image should be displayed (VISUAL)
+* login button should go to either dashboard or employee action page (FUNCTIONAL)
+* create account button should create a new account where user data can be stored (FUNCTIONAL)
 */
+
