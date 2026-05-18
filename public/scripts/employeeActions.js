dbbc75748e7876aa42155c5b6e848d9879b11c97
Rachel G <rachel.m.greenlee@icloud.com>
2026-05-18 17:09:03 -0400

    fixed!

--- public/scripts/employeeActions.js
+++ public/scripts/employeeActions.js
@@ -6,6 +6,6 @@
 * remove button should remove a pet from the database
 */
 
-document.getElementById("add").addEventListener(onclick, function(){
-    window.location.href="http://csse280-08.csse.rose-hulman.edu:8080/add_pet.html";
+document.getElementById("add").addEventListener("click", function(){
+    window.location.href = "http://csse280-08.csse.rose-hulman.edu:8080/add_pet.html";
 });
